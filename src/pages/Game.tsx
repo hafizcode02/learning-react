a044a752ce9231143b9a3fcde58f93c493fd544a
Hafiz Caniago <hafizcode02@gmail.com>
2024-11-27 22:03:18 +0700

    feat: update Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,5 +1,45 @@
+import React, { useState } from "react";
+
+const Square = ({ ...props }: React.HTMLAttributes<HTMLButtonElement>) => {
+  const [value, setValue] = useState(String);
+
+  const clickHandler = () => {
+    setValue("X");
+  };
+
+  return (
+    <button
+      className="w-12 h-12 m-1 shadow-md rounded-lg transition bg-gray-300"
+      {...props}
+      onClick={clickHandler}
+    >
+      {value}
+    </button>
+  );
+};
 function Game() {
-  return <div>Hello World</div>;
+  return (
+    <div className="min-h-screen bg-gray-100 flex gap-10 items-center justify-center">
+      <div className="flex flex-col"></div>
+      <div className="flex flex-col">
+        <div>
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div>
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div>
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Game;
